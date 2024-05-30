9b6198c20afc56a646df14cc8c0940b911008ff3
chenyj <chenyj@jiuyunso.com>
2024-05-30 19:04:24 +0800

    feat(3rd-party): add iniparser

--- tools/pre_push.sh
+++ tools/pre_push.sh
@@ -6,19 +6,19 @@ ROOTDIR=$PWD/../
 
 echo "checking for gcc"
 cd $ROOTDIR
-./cmake_gen.sh gcc.cmake MinSizeRel 1 $ROOTDIR/build/release
+./cmake_gen.sh gcc.cmake MinSizeRel 1 $ROOTDIR/build/install
 cd $ROOTDIR/build
 make -j32 && make test && make install
 
 echo "checking for arm-linux-gnueabi-gcc"
 cd $ROOTDIR
-./cmake_gen.sh arm-linux-gnueabi.cmake MinSizeRel 1 $ROOTDIR/build/release
+./cmake_gen.sh arm-linux-gnueabi.cmake MinSizeRel 1 $ROOTDIR/build/install
 cd $ROOTDIR/build
 make -j32 && make install
 
 echo "checking for arm-buildroot-linux-musleabi-gcc"
 cd $ROOTDIR
-./cmake_gen.sh arm-buildroot-linux-musleabi.cmake MinSizeRel 1 $ROOTDIR/build/release
+./cmake_gen.sh arm-buildroot-linux-musleabi.cmake MinSizeRel 1 $ROOTDIR/build/install
 cd $ROOTDIR/build
 make -j32 && make install
 
