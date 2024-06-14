8e40594c32900192d05f6f9bdc8e6ba22f17cecc
chenyj <chenyj@jiuyunso.com>
2024-06-14 14:01:55 +0800

    build(cmake): update toolchain

--- cmake/toolchain/toolchain-flags.cmake
+++ cmake/toolchain/toolchain-flags.cmake
@@ -4,7 +4,7 @@ get_filename_component(toolchain_we ${CMAKE_TOOLCHAIN_FILE} NAME_WE ABSOLUTE)
 set(flags_file ${PROJECT_SOURCE_DIR}/cmake/toolchain/${toolchain_we}-flags.cmake CACHE STRING "" FORCE)
 
 if((NOT EXISTS ${flags_file}) OR (IS_DIRECTORY ${flags_file}))
-	MESSAGE(FATAL_ERROR  "${flags_file} is not exists")
+	MESSAGE(STATUS  "${flags_file} is not exists")
 else()
 	include(${flags_file})
 endif()
